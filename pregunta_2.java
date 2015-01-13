f268786755a6745dee72fcb1795bb31868688a34
Javier Lopez <javierlopm@hotmail.com>
2015-01-12 23:54:15 -0430

    Agregada estructura del ejercicio 2

--- pregunta_2.java
+++ pregunta_2.java
@@ -1,2 +1,16 @@
+import java.util.*;
 
+class Main{
+    public static void main(String[] args){
+        //Crear arreglo de booleanos
+        //Donde 0 -> A= 65 y 25 ->Z 90
+
+        //Leer archivo y verificar si hay letras seguidas repetidas
+        //Si las encuentra, eliminarlas de la lista
+
+        //Hacer conversion explicitar de caracter a (int)
+
+        System.out.println("Hello world");
+    }
+}
 
